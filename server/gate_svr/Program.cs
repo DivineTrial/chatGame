7bbfa43a225993ee6d5d97dee798c7d2f802899d
吴铭 <451517996@qq.com>
2024-12-01 21:08:57 +0800

    升级到.net8.0 添加初始服务器工程

---
+++ server/gate_svr/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace gate_svr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _gate = new Gate.GateService(args[0], args[1]);
+
+            Log.Log.trace("gate_svr start ok");
+
+            _gate.run().Wait();
+        }
+    }
+}
---
+++ server/room/Program.cs
@@ -0,0 +1,28 @@
+﻿using Abelkhan;
+
+
+namespace Room
+{
+    class Room
+    {
+        static void Main(string[] args)
+        {
+            var _hub = new Hub.Hub(args[0], args[1], "player", "dynamic");
+
+            _hub.on_hubproxy += on_hubproxy;
+            _hub.on_hubproxy_reconn += on_hubproxy;
+
+            _hub.onCloseServer += () => {
+                _hub.closeSvr();
+            };
+
+            Log.Log.trace("player start ok");
+
+            _hub.run().Wait();
+        }
+
+        private static void on_hubproxy(Hub.HubProxy _proxy)
+        {
+        }
+    }
+}
